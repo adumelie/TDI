de28132b7ca87794a66cf82fe0ca41c1d7178563
adumelie <git@spes.simplelogin.com>
2023-10-11 17:14:28 +0200

    Finding serial port

--- plot.py
+++ plot.py
@@ -5,12 +5,33 @@ import sys
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
 import serial
+from datetime import datetime
 
 DEBUG = 0
+LOG_FILE = "LOGS/" + str(datetime.now()).replace(" ", "_")
+LOGGING_DATA = []
+
+#------------------------------
+import glob
+tty_files = glob.glob('/dev/tty*')
+acm_files = [file for file in tty_files if 'ACM' in file]
+if acm_files:
+    port = acm_files[0]
+else:
+    print("No tty available !")
+#------------------------------
 
 import random
 def dummy_read():
    return random.random() * 3
+
+def on_close(event):
+    print("Window is being closed")
+    print("Logging data")
+    with open(LOG_FILE, "w") as f:
+        for item in LOGGING_DATA:
+            f.write(str(item) + "\n")
+    event.accept() 
 
 def read_serial_data(ser):
     data = ser.readline().decode().strip()
@@ -24,36 +45,37 @@ def update():
         value = dummy_read()
     if DEBUG >= 1:
         print(value)
-
     data_y = data_y[1:]  # Remove first element
     data_y.append(value)
 
+    LOGGING_DATA.append(value)
     curve.setData(data_x, data_y)
 #------------------------------
 app = QtWidgets.QApplication([])
 
 win = pg.GraphicsLayoutWidget()
 win.setWindowTitle("Real-Time Data Plotting")
+win.closeEvent = on_close
 win.show()
 
 y_min = 0
 y_max = 3.3
 step = 10 # In ms
 
-plot = win.addPlot(title="Real-Time Data Plot")
+plot = win.addPlot(title="Real-time data plot from sensor")
 plot.setYRange(y_min, y_max)
 
 YAXIS = "left"
 XAXIS = "bottom"
 plot.setLabel(YAXIS, "Voltage")
-plot.setLabel(XAXIS, "Time (update " + str(step) +" ms")
+plot.setLabel(XAXIS, "Time (update " + str(step) +" ms)")
 plot.setTitle("Input data - FSR Glove")
 
 curve = plot.plot(pen=pg.mkPen(color='r'), width=15)
 data_x = [i for i in range(step * 100)]
 data_y = [0 for _ in data_x]
 
-ser = serial.Serial('/dev/ttyACM0', 9600) 
+ser = serial.Serial(port, 9600) 
 
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
