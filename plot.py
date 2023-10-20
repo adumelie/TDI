8a488d6013d820ae7c334af489e59fa2a0528984
adumelie <git@spes.simplelogin.com>
2023-10-20 20:41:41 +0200

    Dry run option

--- plot.py
+++ plot.py
@@ -6,6 +6,7 @@ import sys
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtWidgets
 import serial
+import time
 from datetime import datetime
 
 DEBUG = 0
@@ -19,16 +20,19 @@ N = step * 100
 
 TRIGGERED = False
 TIME_START = N * 1 # To not directly play sound before pressing
-THRESHOLD = 0.4
+THRESHOLD = 0.3
 
 #------------------------------
 import glob
-tty_files = glob.glob('/dev/tty*')
-acm_files = [file for file in tty_files if 'ACM' in file]
-if acm_files:
-    port = acm_files[0]
-else:
-    print("No tty available !")
+def detect_tty():
+    tty_files = glob.glob('/dev/tty*')
+    acm_files = [file for file in tty_files if 'ACM' in file]
+    if acm_files:
+        port = acm_files[0]
+    else:
+        print("No tty available !")
+        port = None
+    return port
 #------------------------------
 
 import random
@@ -72,37 +76,48 @@ def trigger_event(values):
         if len(LOGGING_DATA) > TIME_START:
             array = np.array(values)
             if (array < THRESHOLD).all():
-                sound = AudioSegment.from_file('sound.wav', format='wav')
-                play(sound)
+                triggered()
                 TRIGGERED = True
+def triggered():
+    # time.sleep(60*3)
+    sound = AudioSegment.from_file('sound.wav', format='wav')
+    play(sound)
+
 #------------------------------
-app = QtWidgets.QApplication([])
+def setup():
+    app = QtWidgets.QApplication([])
 
-win = pg.GraphicsLayoutWidget()
-win.setWindowTitle("Real-Time Data Plotting")
-win.closeEvent = on_close
-win.show()
+    win = pg.GraphicsLayoutWidget()
+    win.setWindowTitle("Real-Time Data Plotting")
+    win.closeEvent = on_close
+    win.show()
 
 
-plot = win.addPlot(title="Real-time data plot from sensor")
-plot.setYRange(y_min, y_max)
+    plot = win.addPlot(title="Real-time data plot from sensor")
+    plot.setYRange(y_min, y_max)
 
-YAXIS = "left"
-XAXIS = "bottom"
-plot.setLabel(YAXIS, "Voltage")
-plot.setLabel(XAXIS, "Time (update " + str(step) +" ms)")
-plot.setTitle("Input data - FSR Glove")
+    YAXIS = "left"
+    XAXIS = "bottom"
+    plot.setLabel(YAXIS, "Voltage")
+    plot.setLabel(XAXIS, "Time (update " + str(step) +" ms)")
+    plot.setTitle("Input data - FSR Glove")
 
-curve = plot.plot(pen=pg.mkPen(color='r'), width=15)
-data_x = [i for i in range(N)]
-data_y = [0 for _ in data_x]
+    curve = plot.plot(pen=pg.mkPen(color='r'), width=15)
+    data_x = [i for i in range(N)]
+    data_y = [0 for _ in data_x]
 
-ser = serial.Serial(port, 9600) 
+    ser = serial.Serial(port, 9600) 
 
-timer = QtCore.QTimer()
-timer.timeout.connect(update)
-timer.start(step) 
+    timer = QtCore.QTimer()
+    timer.timeout.connect(update)
+    timer.start(step) 
 #------------------------------
+DRY_RUN = False 
 if __name__ == '__main__':
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+    if DRY_RUN:
+        print("WARNING: DRY RUN")
+        triggered()
+    elif (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        port = detect_tty()
+        app = setup()
         sys.exit(app.exec_())
